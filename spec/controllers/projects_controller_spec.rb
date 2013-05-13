9d90a9c0899b599b54c81614e6d31a262eff7f9d
anromeo <anthonykzullo@gmail.com>
2013-05-13 13:57:01 -0700

    Implement creating tickets for a project

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe ProjectsController do
   it "displays an error for a missing project" do
     get :show, :id => "not-here"
-    binding.pry
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
     flash[:alert].should == message
