1e0dfb55144dfb97f736eab827442af961a062c3
anromeo <anthonykzullo@gmail.com>
2013-05-19 18:54:22 -0700

    Restrict actions in TicketsController based on permissions and hide links

--- spec/integration/deleting_tickets_spec.rb
+++ spec/integration/deleting_tickets_spec.rb
@@ -7,6 +7,7 @@ feature 'Deleting tickets' do
     ticket.update_attribute(:user, user)
     ticket
   end
+
   before do
     define_permission!(user, "view", project)
     define_permission!(user, "delete tickets", project)
@@ -15,6 +16,7 @@ feature 'Deleting tickets' do
     click_link project.name
     click_link ticket.title
   end
+
   scenario "Deleting a ticket" do
     click_link "Delete Ticket"
     page.should have_content("Ticket has been deleted.")
